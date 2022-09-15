12a881485828257eedd986b0c8c540be8a77bf71
Cameron Auler <cameron.auler@mymail.champlain.edu>
2022-09-15 12:37:48 -0400

    organizing functions into modules

--- Code/functions.py
+++ Code/functions.py
@@ -5,7 +5,42 @@
 """This module contains all of the hidden UI functions for the RTTK project."""
 
 import os
+import time
 from sys import platform
+import menus
+import options
+
+def main_title():
+    """This function prints the ASCII logo."""
+    clear()
+
+    print(r"""
+          _____                _____                _____                      _____          
+         /\    \              /\    \              /\    \                    /\    \         
+        /::\    \            /::\    \            /::\    \                  /::\____\        
+       /::::\    \           \:::\    \           \:::\    \                /:::/    /        
+      /::::::\    \           \:::\    \           \:::\    \              /:::/    /         
+     /:::/\:::\    \           \:::\    \           \:::\    \            /:::/    /          
+    /:::/__\:::\    \           \:::\    \           \:::\    \          /:::/____/           
+   /::::\   \:::\    \          /::::\    \          /::::\    \        /::::\    \           
+  /::::::\   \:::\    \        /::::::\    \        /::::::\    \      /::::::\____\________  
+ /:::/\:::\   \:::\____\      /:::/\:::\    \      /:::/\:::\    \    /:::/\:::::::::::\    \ 
+/:::/  \:::\   \:::|    |    /:::/  \:::\____\    /:::/  \:::\____\  /:::/  |:::::::::::\____\ 
+\::/   |::::\  /:::|____|   /:::/    \::/    /   /:::/    \::/    /  \::/   |::|~~~|~~~~~     
+ \/____|:::::\/:::/    /   /:::/    / \/____/   /:::/    / \/____/    \/____|::|   |          
+       |:::::::::/    /   /:::/    /           /:::/    /                   |::|   |          
+       |::|\::::/    /   /:::/    /           /:::/    /                    |::|   |          
+       |::| \::/____/    \::/    /            \::/    /                     |::|   |          
+       |::|  ~|           \/____/              \/____/                      |::|   |          
+       |::|   |                                                             |::|   |          
+       \::|   |                                                             \::|   |          
+        \:|   |                                                              \:|   |          
+         \|___|                                                               \|___|          
+
+----------------------------------------------------------------------------------------------
+______________________________________________________________________________________________
+----------------------------------------------------------------------------------------------
+      """)
 
 def clear():
     """This function clears the terminal window."""
@@ -17,3 +52,29 @@ def clear():
         os.system('cls')
     else:
         return platform
+
+def user_input():
+    """This function gathers the user input."""
+    user_entry = str(input("\n<>   COMMAND: "))
+    user_selection = user_entry.upper()
+
+    if user_selection == "ANONYMITY" or user_selection == "A":
+        menus.anonymity_menu()
+    elif user_selection == "OSINT" or user_selection == "O":
+        menus.osint_menu()
+    elif user_selection == "PROBE" or user_selection == "P":
+        menus.probe_menu()
+    elif user_selection == "ATTACK" or user_selection == "ATk":
+        menus.attack_menu()
+    elif user_selection == "NOTES" or user_selection == "N":
+        menus.notes_menu()
+    elif user_selection == "OPTIONS" or user_selection == "OPT":
+        menus.display_options()
+    else:
+        print("")
+        print("Unrecognized attack phase . . .")
+        for _each_number in range(options.options("menu_length")):
+            time.sleep(0.001)
+            print("=", end="")
+        print("")
+        user_input()
--- Code/main.py
+++ Code/main.py
@@ -4,74 +4,14 @@
 
 """This is the main menu module for the RTTK project."""
 
-import time
-import options
 import menus
 import functions
 
-def main_title():
-    """This function prints the ASCII logo."""
-    functions.clear()
-
-    print(r"""
-          _____                _____                _____                      _____          
-         /\    \              /\    \              /\    \                    /\    \         
-        /::\    \            /::\    \            /::\    \                  /::\____\        
-       /::::\    \           \:::\    \           \:::\    \                /:::/    /        
-      /::::::\    \           \:::\    \           \:::\    \              /:::/    /         
-     /:::/\:::\    \           \:::\    \           \:::\    \            /:::/    /          
-    /:::/__\:::\    \           \:::\    \           \:::\    \          /:::/____/           
-   /::::\   \:::\    \          /::::\    \          /::::\    \        /::::\    \           
-  /::::::\   \:::\    \        /::::::\    \        /::::::\    \      /::::::\____\________  
- /:::/\:::\   \:::\____\      /:::/\:::\    \      /:::/\:::\    \    /:::/\:::::::::::\    \ 
-/:::/  \:::\   \:::|    |    /:::/  \:::\____\    /:::/  \:::\____\  /:::/  |:::::::::::\____\ 
-\::/   |::::\  /:::|____|   /:::/    \::/    /   /:::/    \::/    /  \::/   |::|~~~|~~~~~     
- \/____|:::::\/:::/    /   /:::/    / \/____/   /:::/    / \/____/    \/____|::|   |          
-       |:::::::::/    /   /:::/    /           /:::/    /                   |::|   |          
-       |::|\::::/    /   /:::/    /           /:::/    /                    |::|   |          
-       |::| \::/____/    \::/    /            \::/    /                     |::|   |          
-       |::|  ~|           \/____/              \/____/                      |::|   |          
-       |::|   |                                                             |::|   |          
-       \::|   |                                                             \::|   |          
-        \:|   |                                                              \:|   |          
-         \|___|                                                               \|___|          
-
-----------------------------------------------------------------------------------------------
-______________________________________________________________________________________________
-----------------------------------------------------------------------------------------------
-      """)
-
-def user_input():
-    """This function gathers the user input."""
-    user_entry = str(input("\n<>   COMMAND: "))
-    user_selection = user_entry.upper()
-
-    if user_selection == "ANONYMITY" or user_selection == "A":
-        menus.anonymity_menu()
-    elif user_selection == "OSINT" or user_selection == "O":
-        menus.osint_menu()
-    elif user_selection == "PROBE" or user_selection == "P":
-        menus.probe_menu()
-    elif user_selection == "ATTACK" or user_selection == "ATk":
-        menus.attack_menu()
-    elif user_selection == "NOTES" or user_selection == "N":
-        menus.notes_menu()
-    elif user_selection == "OPTIONS" or user_selection == "OPT":
-        menus.display_options()
-    else:
-        print("")
-        print("Unrecognized attack phase . . .")
-        for _each_number in range(options.options("menu_length")):
-            time.sleep(0.001)
-            print("=", end="")
-        print("")
-        user_input()
-
 def main():
     """__main__"""
-    main_title()
+    functions.main_title()
     menus.main_menu()
-    user_input()
+    functions.user_input()
 
 if __name__ == "__main__":
     main()
