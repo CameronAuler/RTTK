6d330464772f2c0b51ee44b621d5eb5085f55266
Cameron Auler <cameron.auler@mymail.champlain.edu>
2022-10-05 13:12:04 -0400

    initial brutus tool code

---
+++ Code/brutus.py
@@ -0,0 +1,25 @@
+import random
+import string
+import itertools
+import tqdm
+import time
+
+characters = string.printable[:95]
+char_length = 8
+
+def passwd_gen(char_length):
+    init_pass = ""
+    while len(init_pass) < char_length:
+        init_pass += random.choice(characters)
+    return init_pass
+
+def brute_force(passwd, alphabet):
+    start = time.time()
+    passwd_tuple = tuple(passwd)
+    char_list = [[char for char in alphabet]] * len(passwd)
+    args = [char for char in char_list]
+    
+    for combination in itertools.product(*args):
+        if combination == passwd_tuple:
+            print(f"found {passwd_tuple}")
+    stop = time.time()
