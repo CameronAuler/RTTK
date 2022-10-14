a45b98cc4e17cb8039754a54c5b7dc22b7bfecbd
Cameron Auler <cameron.auler@mymail.champlain.edu>
2022-10-14 10:35:17 -0400

    example script from sec480 exploit

--- Code/portscan.py
+++ Code/portscan.py
@@ -38,3 +38,75 @@ def main():
 
 main()
 end = input("Press any key to close")
+
+
+
+
+from queue import Queue
+import socket
+import threading
+import sys
+
+def target():
+        if len(sys.argv) == 2:
+                target = sys.argv[1]
+        else:
+                print("Target argument unfulfilled")
+        return target
+
+queue = Queue()
+open_ports = []
+
+def portscan(port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((target(), port))
+        return True
+    except:
+        return False
+
+def get_ports(mode):
+    if mode == 1:
+        for port in range(1, 1024):
+            queue.put(port)
+    elif mode == 2:
+        for port in range(1, 49152):
+            queue.put(port)
+    elif mode == 3:
+        ports = [20, 21, 22, 23, 25, 53, 80, 110, 443]
+        for port in ports:
+            queue.put(port)
+    elif mode == 4:
+        ports = input("Enter your ports (seperate by blank):")
+        ports = ports.split()
+        ports = list(map(int, ports))
+        for port in ports:
+            queue.put(port)
+
+def worker():
+    while not queue.empty():
+        port = queue.get()
+        if portscan(port):
+            print("Port {} is open!".format(port))
+            open_ports.append(port)
+        else:
+            print("Port {} is closed!".format(port))
+
+def run_scanner(threads, mode):
+        get_ports(mode)
+        thread_list = []
+        for t in range(threads):
+                thread = threading.Thread(target=worker)
+                thread_list.append(thread)
+        for thread in thread_list:
+                thread.start()
+        for thread in thread_list:
+                thread.join()
+        print("Open ports are:")
+        for port in open_ports:
+                try:
+                        service = socket.getservbyport(port)
+                        print(f"port {port} running {service}")
+                except:
+                        print(f"port {port} running an unknown service")
+run_scanner(100, 2)
