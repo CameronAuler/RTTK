aa4818243290819ddbd1e38c5fe77fb83f27fa7d
Cameron Auler <cameron.auler@mymail.champlain.edu>
2022-11-04 17:52:56 -0400

    init file change

--- Code/tools/__init__.py
+++ Code/tools/__init__.py
@@ -1,1 +1,7 @@
+# Package modualization statement
 __all__ = ["brutus", "crack", "cvedb", "dnum", "dos", "net_scan", "proxy_pong", "pyfi", "squeegee", "vscan"]
+
+# Required Dependancies
+"""
+    - nmap(python)
+"""
