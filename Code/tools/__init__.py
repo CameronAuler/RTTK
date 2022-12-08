4c1ae2c68c1d87d712453ba7b2632f2b4d68fbb6
Cameron Auler <70597465+CameronAuler@users.noreply.github.com>
2022-12-08 12:13:36 -0500

    Update __init__.py

--- Code/tools/__init__.py
+++ Code/tools/__init__.py
@@ -1,7 +1,2 @@
 # Package modualization statement
 __all__ = ["brutus", "crack", "cvedb", "dnum", "dos", "net_scan", "proxy_pong", "pyfi", "squeegee", "vscan"]
-
-# Required Dependancies
-"""
-    - nmap(python)
-"""
